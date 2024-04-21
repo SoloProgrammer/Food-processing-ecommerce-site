b8bd6d09d12c5c34cbd22579977fcb472098f371
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-22 02:06:05 +0530

    Added new types Product and ProductCategory

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,33 @@ export type ImageType = {
   src: string;
   title: string;
 };
+
+export type ProductCategory = {
+  title: string;
+  short_desc: string;
+  image: string;
+  slug: string;
+};
+
+export type Product = {
+  images: string[];
+  title: string;
+  details: {
+    "Nature of products": string;
+    "Packing size": Object;
+    Ingredients: string;
+    Specifications: {
+      TSS: string;
+      Acidity: string;
+      pH: string;
+      Salt?: string;
+    };
+    "Nutritional values per 100gm of product": {
+      Energy: string;
+      Carbohydrates: string;
+      Proteins: string;
+      Fats: string;
+      Sugar: string;
+    };
+  };
+};
