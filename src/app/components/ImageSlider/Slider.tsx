a7774e26ceb8e8f8ddc3ccd250f1b07d0cbf5776
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-21 13:44:41 +0530

    Refactor ImageSlider code & logic

--- src/app/components/ImageSlider/Slider.tsx
+++ src/app/components/ImageSlider/Slider.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { ImageType } from "@/types/types";
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  LegacyRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import styles from "./Slider.module.css";
 import Image from "next/image";
@@ -12,27 +18,24 @@ type SliderProps = {
 const Slider = ({ images }: SliderProps) => {
   const [currSlide, setCurrSlide] = useState(1);
   const inidicatorRef = useRef<HTMLDivElement>(null);
+
+  // Initiating tab indicator default position 
   useEffect(() => {
     if (inidicatorRef.current) {
       inidicatorRef.current.style.left = "8px";
       inidicatorRef.current.style.width = "165px";
     }
   }, [inidicatorRef.current]);
+
   const handleSlideChange = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
     i: number
   ) => {
-    const target = e.target as HTMLDivElement;
-    const left = target.offsetLeft;
-    const width = target.offsetWidth;
-    if (inidicatorRef.current) {
-      inidicatorRef.current.style.left = `${left}px`;
-      inidicatorRef.current.style.width = `${width}px`;
-      setCurrSlide(i + 1);
-    }
+    setCurrSlide(i + 1);
   };
+  
   const tabRefs = useRef<HTMLDivElement[]>([]);
-
+  // autoplay slider logic
   useEffect(() => {
     const sliderTimer = setInterval(() => {
       setCurrSlide((prev) => (prev > 3 ? 1 : prev + 1));
@@ -43,6 +46,7 @@ const Slider = ({ images }: SliderProps) => {
     };
   }, [currSlide]);
 
+  // autoplay indicator tab logic
   useEffect(() => {
     const currTab = tabRefs.current[currSlide - 1] as HTMLDivElement;
     if (inidicatorRef.current) {
@@ -51,7 +55,8 @@ const Slider = ({ images }: SliderProps) => {
     }
   }, [tabRefs, currSlide, inidicatorRef]);
 
-  const ImageSlideOffset = `translate-x-[-${(currSlide - 1) * 100}%]`;
+  // ImageSliderOffset value
+  const ImageSlideOffset = `translateX(-${(currSlide - 1) * 100}%)`;
 
   return (
     <div className={`${styles.container}`}>
@@ -59,38 +64,62 @@ const Slider = ({ images }: SliderProps) => {
         {images.map((img) => (
           <div
             key={img.title}
-            className={`${ImageSlideOffset} flex-shrink-0 ${styles.imageBox} relative w-full `}
+            style={{
+              transform: ImageSlideOffset,
+            }}
+            className={`flex-shrink-0 ${styles.imageBox} relative w-full `}
           >
             <Image fill src={img.src} alt={img.title} />
           </div>
         ))}
       </div>
+      <SliderTabs
+        handleTabChange={handleSlideChange}
+        tabs={images.map((img) => img.title)}
+        tabRefs={tabRefs}
+        ref={inidicatorRef}
+      />
+    </div>
+  );
+};
+
+type SliderTabsProps = {
+  handleTabChange: (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    i: number
+  ) => void;
+  tabs: String[];
+  tabRefs: React.MutableRefObject<HTMLDivElement[]>;
+};
+
+const SliderTabs = forwardRef(
+  (
+    { handleTabChange, tabs, tabRefs }: SliderTabsProps,
+    indicatorRef: LegacyRef<HTMLDivElement>
+  ) => {
+    return (
       <div className="hidden md:flex w-full justify-center mt-2">
         <div className="flex items-center lg:gap-4 md:gap-1 mx-2 px-2 rounded-lg justify-center bg-gray-100 py-2 overflow-x-auto relative border-solid border-[1px] border-gray-300">
-          {images
-            .map((img) => img.title)
-            .map((t, i) => (
-              <div
-                ref={(el) => {
-                  tabRefs.current[i] = el!;
-                }}
-                onClick={(e) => {
-                  handleSlideChange(e, i);
-                }}
-                key={t}
-                className={`md:px-7  lg:px-10 py-1 rounded-md cursor-pointer whitespace-nowrap z-10`}
-              >
-                {t}
-              </div>
-            ))}
+          {tabs.map((t, i) => (
+            <div
+              ref={(el) => {
+                tabRefs.current[i] = el!;
+              }}
+              onClick={(e) => handleTabChange(e, i)}
+              key={i}
+              className={`md:px-7  lg:px-10 py-1 rounded-md cursor-pointer whitespace-nowrap z-10`}
+            >
+              {t}
+            </div>
+          ))}
           <div
-            ref={inidicatorRef}
+            ref={indicatorRef}
             className={`${styles.indicator} bg-white shadow-md h-[70%] absolute rounded-lg duration-[.3s]`}
           />
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Slider;
