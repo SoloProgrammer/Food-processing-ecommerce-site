807b718d691995af6979b7d732cf4178207b4405
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-24 03:03:58 +0530

    modified types file

--- src/types/types.ts
+++ src/types/types.ts
@@ -10,28 +10,33 @@ export type ProductCategory = {
   slug: string;
 };
 
+type ProductSpecifications = {
+  TSS: string;
+  Acidity: string;
+  pH: string;
+  Salt: string;
+};
+
+type ProductNutritionalValues = {
+  Energy: string;
+  Carbohydrates: string;
+  Proteins: string;
+  Fats: string;
+  Sugar: string;
+  "Vitamin-A": string;
+  "Vitamin-C": string;
+};
+
+export type ProductDetails = {
+  "Nature of products": string;
+  Ingredients: string;
+  Specifications: Partial<ProductSpecifications>;
+  "Nutritional values per 100gm of product": Partial<ProductNutritionalValues>;
+};
+
 export type Product = {
   images: string[];
   title: string;
   cat_slug: string;
-  details: {
-    "Nature of products"?: string;
-    Ingredients?: string;
-    Specifications?: {
-      TSS: string;
-      Acidity: string;
-      pH: string;
-      Salt?: string;
-    };
-    "Nutritional values per 100gm of product"?: {
-      Energy?: string;
-      Carbohydrates?: string;
-      Proteins?: string;
-      Fats?: string;
-      Sugar?: string;
-      "Vitamin-A"?: string;
-      "Vitamin-C"?: string;
-    };
-    Uses?: string;
-  };
+  details: Partial<ProductDetails>;
 };
