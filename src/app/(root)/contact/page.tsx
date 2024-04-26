2cf48d526aaf78513a2e60e3f974708c51ae317b
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-26 16:38:49 +0530

    created contact page

---
+++ src/app/(root)/contact/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import React from "react";
+import ContactImage from "@/assests/images/contact.png";
+
+const page = () => {
+  return (
+    <div className="flex w-full items-center justify-center py-0 md:py-10 md:gap-10 flex-wrap">
+      <div className="w-[400px]">
+        <Image
+          src={ContactImage}
+          alt="contact"
+          fill
+          className="!relative object-contain"
+        />
+      </div>
+      <div className="bg-white flex flex-col w-full max-w-[600px] px-5">
+        <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+          Feedback
+        </h2>
+        <p className="leading-relaxed mb-5 text-gray-600">
+          Post-ironic portland shabby chic echo park, banjo fashion axe
+        </p>
+        <div className="relative mb-4">
+          <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="message" className="leading-7 text-sm text-gray-600">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+          ></textarea>
+        </div>
+        <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          Submit
+        </button>
+        <p className="text-xs text-gray-500 mt-3">
+          Chicharrones blog helvetica normcore iceland tousled brook viral
+          artisan.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
