67eee8057352777527794e9feca84e67f942d6bc
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-28 15:14:43 +0530

    imported home page sliderImages from json file

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,30 +3,13 @@ import CategoriesSlider from "@/components/CategoriesSlider/CategoriesSlider";
 import BannerSlider from "@/components/ImageSlider/Slider";
 import Divider from "@/components/Divider/Divider";
 import { aboreto } from "@/fonts/fonts";
+import sliderImages from "@/data/sliderImages.json";
 
 const HomePage = () => {
-  const sliderImages = [
-    {
-      title: "Mongo Pulp",
-      src: "http://aditifoods.com/images/main_banner/main_banner_1.jpg",
-    },
-    {
-      title: "Ketchups & Jams",
-      src: "http://aditifoods.com/images/main_banner/main_banner_2.jpg",
-    },
-    {
-      title: "Canned Products",
-      src: "http://aditifoods.com/images/main_banner/main_banner_3.jpg",
-    },
-    {
-      title: "Private Labels",
-      src: "http://aditifoods.com/images/main_banner/main_banner_4.jpg",
-    },
-  ];
   return (
     <div>
-      <BannerSlider images={sliderImages} autoPlay/>
-      <Divider className="px-5 my-7 md:px-20 md:my-14"/>
+      <BannerSlider images={sliderImages} autoPlay />
+      <Divider className="px-5 my-7 md:px-20 md:my-14" />
       <h1
         className={`${aboreto.className} text-2xl md:text-3xl px-4 md:px-20 mt-4 text-gray-700 font-bold`}
       >
