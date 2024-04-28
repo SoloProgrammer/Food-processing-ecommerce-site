08e4fda32e1358ffcd63a8b4fedfdd3cfb1bc1ab
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-28 14:20:20 +0530

    creates a utility function isMobileScreen that returns true if the window width/size is equal to mobile screen

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function isMobileScreen() {
+  if (typeof window !== "undefined") {
+    return window.innerWidth <= 778;
+  }
+  return false
 }
